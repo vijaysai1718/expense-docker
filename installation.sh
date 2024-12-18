4ca5e07560687f217503aacd3d3cd2ce67520e9a
vijaysai <vijaysai.chinthala45@gmail.com>
2024-12-18 11:38:07 +0530

    docker

--- installation.sh
+++ installation.sh
@@ -10,4 +10,4 @@ yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-co
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
-echo -e "$R Logout and Login again $N"
+echo -e "$R Logout and Login again please $N"
